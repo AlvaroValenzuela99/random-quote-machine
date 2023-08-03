8288f5c428a511f5e621cecbfc59897e9b8c9f59
Alvaro Valenzuela <alvarovgerena@gmail.com>
2023-08-03 11:52:36 +0200

    finished project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import QuoteBox from './components/QuoteBox'
 
 function App() {
   return (
     <div className="App">
-      
+      <QuoteBox />
     </div>
   );
 }
--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -5,24 +5,83 @@ class QuoteBox extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            quote: ''
+            randomIndex: 0
         }
+        this.newQuote = this.newQuote.bind(this);
     }
+
+    newQuote(){
+        this.setState({
+            randomIndex: Math.floor(Math.random()*10)
+        })
+    }
+
     render(){
 
         const possibleQuotes = [
             {
                 quote: 'Nadie me puso alas, pero aprendí a volar. No detuve vuestras balas, pero aprendí a esquivar. Me quisisteis evitar y eliminar, dejándome como un náufrago perdido en la inmensidad del mar',
                 author: 'Nach'
+            },
+            {
+                quote: 'Me ajusto a la vida, pero la vida no es justa, quien me gusta no me quiere y quien me quiere no me gusta',
+                author: 'Juaninacka'
+            },
+            {
+                quote: 'La vida sale cara confío en que la amortices, hablo de ser feliz ¿¡eh!? No dejes que nadie ni nada a ti te desquicie',
+                author: 'The Louk'
+            },
+            {
+                quote: 'Son tan pocos años y tantos recuerdos, tantos peldaños a un cielo que esta en tus ojos, el mundo esta roto llorando en cada nube, versos dedicados a un corazón que nunca tuve',
+                author: 'Dolcce Rotta'
+            },
+            {
+                quote: 'Vivo la edad dorada bro, añorando la del pavo',
+                author: 'Juancho Marqués (Suite Soprano)'
+            },
+            {
+                quote: 'Con un lanzallamas a buscar a los del Ku Klux, van a arder sus sábanas, van a acabar negros. La vida tiene esa magia, qué ironía',
+                author: 'Ayax'
+            },
+            {
+                quote: 'La vida es simple recibes de lo que has dado y para ser feliz tan solo hay que olvidar el pasado',
+                author: 'Kase.O'
+            },
+            {
+                quote: 'Eres tan pobre que solo tienes dinero',
+                author: 'Tote King'
+            },
+            {
+                quote: 'Salgo a la calle y dejo en casa los prejuicios, he saltado tantas veces que conozco el precipicio',
+                author: 'Zatu (SFDK)'
+            },
+            {
+                quote: 'Fui la oveja negra por salirme del rebaño, cinco años después soy el dueño del establo',
+                author: 'Natos (Natos y Waor)'
             }
         ]
 
+        let quoteText = possibleQuotes[this.state.randomIndex].quote;
+        let quoteAuthor = possibleQuotes[this.state.randomIndex].author;
+        
         return (
             <div id="quote-box">
-                <div id="text"></div>
-                <div id="author"></div>
-                <div id="new-quote"></div>
-                <div id="tweet-quote"></div>
+                <div id="text">
+                    <p className="quoteText">{quoteText}</p>
+                </div>
+
+                <div id="author">
+                    <p className="quoteAuthor">- {quoteAuthor}</p>
+                </div>
+
+                <div className="bottom-container">
+
+                    <a target="top" id="tweet-quote" href={`twitter.com/intent/tweet?text=${quoteText}${quoteAuthor}`}>Tweet</a>
+                            
+                    <div id="new-quote">
+                        <button onClick = {this.newQuote}>New quote</button>
+                    </div>
+                </div>
             </div>
         )
     }
